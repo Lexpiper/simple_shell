e7b71e5cff3a89c88898c648fda709cd27304638
ladyfavour039 <ladyf8039@gmail.com>
2023-05-16 06:59:54 -0700

    creates a new string and copies a given string

--- strings_utils.c
+++ strings_utils.c
@@ -34,4 +34,31 @@ int _strcmp(char *sl, char *s2)
 	
 	return (s1[index] - s2[index]);
 }
+/**
+ * _strncmp - Compares two strings up to a certain number of characters
+ * @s1: First string.
+ * @s2: Second string.
+ * @n: Number of characters to compare.
+ *
+ * Return: 0 if both strings are equal, n or -n if they are not.
+ */
+char *_strdup(char *str)
+{
+	int index, len;
+	char *new_str;
+
+	len = _strlen(str);
+
+	new_str = malloc((len + 1) * sizeof(char));
+
+	if (new_str == NULL)
+		return (NULL);
+	for (index = 0; index < len; index++)
+	{
+		new_str[index] = str[index];
+	}
+	new_str[index] = '\0';
+
+	return (new_str);
+}
 
