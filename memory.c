c8a7a6b2ea378b24dedc40a34266452687597748
ladyfavour039 <ladyf8039@gmail.com>
2023-05-16 05:54:05 -0700

    memoryfile

---
+++ memory.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * _free - Frees memory allocated by malloc accoding to the number of arguments passed
+ * @n: the number of arguments passed to the function
+ * Return: nothing
+ */
+void _free(int n, ...)
+{
+	char **str_array, *str;
+	va_list args;
+	int i;
+
+	va_start(args, n);
+
+	if (n == 1)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			return;
+		free(str);
+	}
+	if (n == 2)
+	{
+		str_array = va_arg(args, char **);
+		if (str_array == NULL)
+			return;
+		for (i = 0; str_array[i] != NULL; i++)
+			free(str_array[i]);
+		free(str-array);
+	}
+
+	va_end(args);
+}
