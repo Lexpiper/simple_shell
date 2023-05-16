71ac078496da3cbb171538957d81e9facb7e98e6
ladyfavour039 <ladyf8039@gmail.com>
2023-05-16 06:39:42 -0700

    compares two strings

--- strings_utils.c
+++ strings_utils.c
@@ -15,3 +15,23 @@ int _strlen(char *str)
 
 	return (len);
 }
+
+/**
+ * _strcmp - Compares two strings character by character.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: Difference between the characters in the strings.
+ */
+int _strcmp(char *sl, char *s2)
+{
+	int index = 0;
+
+	while ((s1[index] != '\0' && s2[index] != '\0') && s1[index] == s2[index])
+	{
+		index++;
+	}
+	
+	return (s1[index] - s2[index]);
+}
+
