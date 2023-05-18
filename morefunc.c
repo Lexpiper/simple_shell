b52b01192dea924b897f6792a66a607a5974d3a2
ladyfavour039 <ladyf8039@gmail.com>
2023-05-18 12:26:50 -0700

    prints an integer

--- morefunc.c
+++ morefunc.c
@@ -24,4 +24,26 @@ void print_errmsg(char **executable, char **args, int count)
 	write(STDERR_FILENO, new_line, _strlen(new_line));
 }
 
+/**
+ * prints_number - prints an integer using write
+ * @n: Integer to be printed
+ * Return: Nothing, void
+ */
+void print_number(int n)
+{
+	int i;
+	char digit;
+	unsigned int num;
+
+	num =(unsigned int)n;
+	i = 1;
+	while ((num / i) > 9)
+		i *= 10;
+	while (i >= 1)
+	{
+		digit = ((num / i) % 10) + '0';
+		write(STDERR_FILE, &digit, 1);
+		i /= 10;
+	}
+}
 
